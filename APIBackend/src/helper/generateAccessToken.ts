4968e9ed3f811c8a98d1ecb622cfc1aca1ab3a01
Hadjira Haya <bkh.hadjira@gmail.com>
2023-12-01 15:56:03 -0500

    Clean files

--- APIBackend/src/helper/generateAccessToken.ts
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generateAccessToken = (user: any) =>
-{
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '180s' })
-  }
-
-  export default generateAccessToken;
